3fe012ebfdc5c5200b7e37e9c6acef8a146b2c40
danielvoutta <kralos.daniel497@gmail.com>
2025-12-07 15:06:28 -0300

    Revert "fix: a exclusão de partes redundantes"
    This reverts commit 7f05468cb76d90247b6dac2bdb46c8a8af0e4e2a.

--- AgendEX.py
+++ AgendEX.py
@@ -102,6 +102,8 @@ def EditarTarefa():
     EditandoTarefa()
 
 
+EditarTarefa()
+
 
 
 
@@ -149,7 +151,7 @@ elif opcao == "3":
 elif opcao == "4":
     print(f"\nOPÇÃO {opcao} SELECIONADA - EDITAR TAREFA  ✏️")
     divisoria()
-    
+    print()
 elif opcao == "5":
     print(f"\nOPÇÃO {opcao} SELECIONADA - REMOVER TAREFA  🗑️")
     divisoria()
