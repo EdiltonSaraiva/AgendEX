e3eb58015ed6d667a45e72dd5149ec2b29094efa
danielvoutta <kralos.daniel497@gmail.com>
2025-12-07 13:08:54 -0300

    feat: editar tarefas

--- AgendEX.py
+++ AgendEX.py
@@ -1,3 +1,117 @@
+TarefasAgendex = []
+
+def divisoria():
+    print("-" * 49)
+
+
+def MensagemSemTarefa():
+            divisoria()
+            print("OPA...PARECE QUE VOCÊ NÃO ADICIONOU NENHUMA TAREFA AO SEU AGENDEX 😅")
+            divisoria()
+            print("Para adicionar uma nova tarefa volte ao Menu Inicial\ne selecione a opção '1 - ADICIONAR NOVA TAREFA'.\nFazendo isso, a partir do momento que uma tarefa\nfor criada, você poderá editá-la.")
+            print("\nDESEJA VOLTAR AO MENU INICIAL?")
+            print("1 - SIM, VOLTAR.")
+            print("2 - NÃO, QUERO FECHAR O AGENDEX.")
+
+            opc_sem_tarefa = input("\nDigite aqui o NÚMERO da ação desejada: ")
+
+            if opc_sem_tarefa == "1":
+                print(f" AÇÃO {opc_sem_tarefa} SELECIONADA - SIM, VOLTAR")
+                MenuInicial()
+            elif opc_sem_tarefa == "2":
+                print(f"AÇÃO SELECIONADA {opc_sem_tarefa} - NÃO, QUERO FECHAR O AGENDEX")
+                divisoria()
+                print("\nSAIU DO PROGRAMA COM SUCESSO!\nOBRIGADO POR UTILIZAR.\n😁📴\n\n")
+            else:
+                divisoria()
+                print("Ação inexistente! Tente novamente!")
+                divisoria()
+                MensagemSemTarefa()
+
+
+def EditarTarefa():
+    def EditandoTarefa():
+        if len(TarefasAgendex) > 0:
+            divisoria()
+            print("POR FAVOR, PARA PROSSEGUIR, SELECIONE A TAREFA QUE VOCÊ DESEJA EDITAR: ")
+            divisoria()
+            for indice_tarefa, tarefa in enumerate(TarefasAgendex, start = 1):
+                print(f"{indice_tarefa} - {tarefa.upper()}")
+
+            indice_usuario = 0
+
+            try: 
+                indice_usuario = int(input("\nDigite o NÚMERO da tarefa que deseja editar: "))
+                divisoria()
+                print(f"VOCÊ ESTÁ EDITANDO A TAREFA DE NÚMERO {indice_usuario} - {TarefasAgendex[indice_usuario - 1]}")
+            except ValueError:
+                divisoria()
+                print("Por favor, digite apenas números! Tente novamente!")
+                divisoria()
+                EditarTarefa()
+            except IndexError:
+                divisoria()
+                print("A tarefa que você tentou acessar, não existe! Tente novamente!")
+                divisoria()
+                EditarTarefa()
+            else:
+                indice_tarefa = indice_usuario - 1
+                divisoria()
+                TarefasAgendex[indice_tarefa] = input("Digite aqui a nova descrição da tarefa: ")
+                divisoria()
+                print("TAREFA ATUALIZADA COM SUCESSO ✅!")
+                print("PARA VER SUAS TAREFAS, VOLTE AO MENU\nINICIAL E SELECIONE A OPÇÃO\n'2 - LISTAR TAREFAS EXISTENTES'")
+                divisoria()
+
+
+                def ContinuarEditando():
+                    print("\nVocê deseja editar alguma tarefa, ou voltar ao menu inicial?")
+                    print("1 - SIM, EDITAR.")
+                    print("2 - NÃO, VOLTAR AO MENU INICIAL.")
+
+                    opc = input("Digite aqui o NÚMERO da ação desejada: ")
+
+                    if opc == "1":
+                        divisoria()
+                        print(f"AÇÃO {opc} SELECIONADA - SIM, EDITAR")
+                        divisoria()
+                        print("\n", " " * 18, "CONTINUAR EDITANDO", " " * 18, "\n")
+                        EditandoTarefa()
+                    elif opc == "2":
+                        divisoria()
+                        print(f"AÇÃO {opc} SELECIONADA - NÃO, VOLTAR AO MENU INICIAL")
+                        divisoria()
+                        MenuInicial()
+                    else:
+                        divisoria()
+                        print("A ação digitada não existe! Tente novamente!")
+                        divisoria()
+                        ContinuarEditando()
+
+
+            ContinuarEditando()
+
+        else:
+            MensagemSemTarefa()
+
+
+    print("\n", " " * 18, "✍️  EDITAR TAREFAS 🧩", " " * 18, "\n")
+    print("BEM-VINDO(A), À SESSÃO DE EDIÇÃO DE TAREFAS DO AGENDEX 📒!")
+    print("SE VOCÊ PRECISA FAZER ALTERAÇÕES EM UMA TAREFA\nQUE VOCÊ CRIOU, ESTÁ NO LUGAR CERTO! EDITE AS\nTAREFAS QUE VOCÊ PRECISA.")
+
+    EditandoTarefa()
+
+
+EditarTarefa()
+
+
+
+
+
+
+
+
+
 print("_" * 49)
 print("\n                  A-G-E-N-D-E-X                  \n")
 print("_" * 49)
@@ -53,3 +167,5 @@ elif opcao == "7":
 else:
     divisoria()
     print("\nERRO! OPÇÃO DIGITADA NÃO EXISTE ❌\n\n")
+
+
